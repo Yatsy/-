5fea3b4d9b59315612ead92bd154d71565625b0b
Yatsy <100126871+Yatsy@users.noreply.github.com>
2022-02-21 12:59:57 +0300

    Add files via upload

---
+++ Задания/Задание 1/ConsoleApplicationc++/ConsoleApplicationc++/ConsoleApplicationc++.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    string text;
+    cin >> text;
+    int a = 0;
+    int b = 0;
+    
+    
+    for (int i = 0; i < size(text); i++)
+    {
+        if (text[0] == ')')
+        {
+            cout << system("cls")<<  "Error ')'" << endl;
+            break;
+        }
+        if (text[i] == ')') a++;    
+        else if (text[i] == '(') b++;
+        else
+            if ( (int(text[i]) < 47 || (int(text[i]) > 57)) && (int(text[i]) < 40 || (int(text[i]) > 43)) && (((int)text[i]) != 45) && (((int)text[i]) != 47) )
+            {
+                cout << system("cls") << "Error 'letters'";
+                break;
+             }
+
+        if ((i == text.length()-1) && (a != b))
+        {
+            cout << system("cls") << "Error '()'";
+        }
+        else if (i == text.length() - 1)
+            cout << "Success" << endl;
+
+        }
+
+
+}
+
